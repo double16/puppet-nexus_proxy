2e5fe767beba68368eab43f46dcb83358dd0ea7c
Patrick Double <pat@patdouble.com>
2017-04-27 06:48:10 -0500

    Gradle 3.5

--- manifests/deployed/thirdparty.pp
+++ manifests/deployed/thirdparty.pp
@@ -28,6 +28,7 @@ class nexus_proxy::deployed::thirdparty {
   nexus_proxy::phantomjs { '2.1.1': }
   nexus_proxy::gradle { '2.14.1': }
   nexus_proxy::gradle { '3.4.1': }
+  nexus_proxy::gradle { '3.5': }
   nexus_proxy::node { '6.10.1': } # LTS
   nexus_proxy::node { '7.7.4': } # Current
   nexus_proxy::grails { '2.3.11': }
